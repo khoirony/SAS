18b1de0db877ce0037948d42adaed5f261b62659
Khoirony Arief <65735480+khoirony@users.noreply.github.com>
2021-05-24 21:06:52 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" href="image/favicon.ico" type="image/x-icon">
+
+    <title>Company Profile PT. Sinar Ahza Samudra</title>
+  </head>
+  <body>
+    
+    <?php include('navbar.php'); ?>
+    <!-- akhir Navbar -->
+
+    <section class="about" id="contact"> 
+    <div class="container">
+        <br><br><br><br><br>
+        <div class="row">
+          <div class="col-md">
+            <div class="list-group-item border-0">
+            <h3 class="text-center">VISI & MISI</h3>
+            <hr>
+            <p class="text-center">PT. SINAR AHZA SAMUDRA Berkomitmen untuk memberikan pelayanan yang maksimal aman dan cepat kepada costumer disetiap kegiatan bongkar muat.</p> 
+            <br><br>
+            <b>VISI</b><br>
+            "Menjadi Perusahaan Bongkar Muat Yang Profesional dan Handal"<br>
+            <br>
+            <b>MISI</b><br>
+            - Menjamin Kepastian Usaha Perusaahaan Bongkar Muat<br>
+            - Meningkatkan Pelayanan Demi Keamanan dan Kelancaran Arus Barang<br>
+            - Menekan Ekonomi Biaya Tinggi di Pelabuhan, Meningkatkan Efisiensi Serta Efektivitas Kerja di Pelabuhan<br>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="list-group-item border-0">
+              <div class="row">
+                <div class="col-4">- Nama Perusahaan <br></div>
+                <div class="col">: PT. SINAR AHZA SAMUDRA <br></div>
+              </div>
+              <div class="row">
+                <div class="col-4">- Alamat <br></div>
+                <div class="col">: Komplek Pelabuhan Brondong No. 08 Sedayulawas, Brondong, Kab. Lamongan, Jawa Timur <br></div>
+              </div>
+              <div class="row">
+                <div class="col-4">- Nomor Telpon <br></div>
+                <div class="col">: 081357969798 / 081231259289 <br></div>
+              </div>
+              <div class="row">
+                <div class="col-4">- Email</div>
+                <div class="col">: sassedayu.ops@gmail.com</div>
+              </div>
+              <div class="row">
+                <div class="col-4">- SIUP <br></div>
+                <div class="col">: P2T/02/06.39/III/2015</div>
+              </div>
+              <div class="row">
+                <div class="col-4">- NPWP</div>
+                <div class="col">: 72.448.496.9-612.000</div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    </section> 
+
+
+
+  
+
+      <br/><br/><br/><br/>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  </body>
+</html>
